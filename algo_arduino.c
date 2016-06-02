b17924e026ec515bdfe390ebd35cf68388c2eb22
pranav1001 <pranavsankhe40@gmail.com>
2016-06-03 00:39:55 +0530

    lets start coding

---
+++ algo_arduino.c
@@ -0,0 +1,30 @@
+/*
+
+Stepper : 
+Stepper myStepper(stepsPerRevolution, 8,9,10,11);
+myStepper.setSpeed()
+myStepper.step()
+
+*/
+
+#include <Stepper.h>
+#include <Stepper.h>
+#include <Wire.h>        
+#include <MPU6050_6Axis_MotionApps20.h>
+#include <MPU6050.h>
+#include <MPU6050_9Axis_MotionApps41.h>
+#include <helper_3dmath.h>
+
+void setup()
+{
+
+}
+
+
+void loop()
+{
+	//take data hall probe sensor. process it. and accordingly chage the output. 
+
+
+}
+
